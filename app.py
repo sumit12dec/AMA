f2c37b16d973da7d7c51810d5ea8132ca907dc25
Sumit Raj <sumit786raj@gmail.com>
2015-08-29 23:51:10 +0530

    new changes

--- app.py
+++ app.py
@@ -6,7 +6,9 @@ import flask
 import csv,sys
 import MySQLdb
 from flask import g
-
+from flask import Flask, render_template, request, redirect, Response, url_for
+import time, os, json, base64, hmac, urllib
+from hashlib import sha1
 
 import time
 from flask.ext.cache import Cache
@@ -34,7 +36,7 @@ def VoiceToText():
   import urllib
   import json
   import requests
-  audiourl = request.args.get('url')
+  location = request.args.get('location')
   speech_url = "https://api.wit.ai/speech?v=20141022"
 
   headers = {
@@ -43,13 +45,54 @@ def VoiceToText():
 
         }
 
-  urllib.urlretrieve(audiourl, "testfile")
-
-  #print requests.post(speech_url, headers=headers, data=file("testfile",'rb').read()).text
-  ares = requests.post(speech_url, headers=headers, data=file("testfile",'rb').read())
+  ares = requests.post(speech_url, headers=headers, data=file(location,'rb').read())
   aresjs = json.loads(ares.text)
+
   print aresjs["_text"]
-  return aresjs["_text"]
+  return str(aresjs["_text"])
+
+
+
+
+@app.route("/UploadFile", methods=["POST"])
+def UploadFile():
+
+  import sys
+  import urllib
+  import json
+  import boto
+  from boto.s3.connection import S3Connection
+  from boto.s3.key import Key
+  import ntpath
+  from werkzeug import secure_filename
+
+
+  aws_key = 'AKIAJ2ML5NXDHZS3WCEA'
+  aws_secret = '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'
+  seqhack = "seqhack2015"
+  file = request.files['file']
+  if file :
+      filename = secure_filename(file.filename)
+      s3 = boto.connect_s3(aws_key, aws_secret)
+      bucket = s3.get_bucket(seqhack)
+      key = bucket.new_key(filename)
+      key.set_contents_from_file(file, headers=None, replace=True, cb=None, num_cb=10, policy=None, md5=None)
+
+      key.make_public()
+
+      url = key.generate_url(expires_in=0, query_auth=False)
+      return url
+
+
+
+
+
+
+
+
+
+
+
 
 if __name__ == "__main__":
 	app.run(debug=True)
