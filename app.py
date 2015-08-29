08712a9da23be93ab8de112ab977e4b8bd4c22be
Sumit Raj <sumit786raj@gmail.com>
2015-08-30 00:36:46 +0530

    new

--- app.py
+++ app.py
@@ -77,13 +77,13 @@ def UploadFile():
       s3 = boto.connect_s3(aws_key, aws_secret)
       bucket = s3.get_bucket(seqhack)
       key = bucket.new_key(filename)
-      key.set_contents_from_file(file, headers=None, replace=True, cb=None, num_cb=10, policy=None, md5=None)
+      #key.set_contents_from_file(file, headers=None, replace=True, cb=None, num_cb=10, policy=None, md5=None)
 
-      time.sleep(5)
+      #time.sleep(5)
 
-      key.make_public()
+      #key.make_public()
 
-      time.sleep(2)
+      #time.sleep(2)
 
 
       #url = key.generate_url(expires_in=0, query_auth=False)
